006d5408601971850453e48461d0733fa5ecbdb6
Kenny Hammerlund <kenny.hammerlund@marriott.com>
2024-08-21 16:22:58 -0500

    feat: add error union with state for not found.

--- src/modules/swapi/resolvers.ts
+++ src/modules/swapi/resolvers.ts
@@ -5,22 +5,40 @@ type Args = { id: string };
 
 const Query: Record<string, GraphQLFieldResolver<{}, Context, any>> = {
   swapiCharacterById: async (_, args: Args, ctx) => {
-    return {};
+    const char = await fetch(`https://swapi.dev/api/people/${args.id}/`);
+
+    if (char.status == 404) {
+      return {
+        content: {
+          message: "Character not found",
+          localizedMessage: "Personaje no encontrado",
+        },
+        __typename: "StarWarsCharacterNotFound",
+      };
+    }
+
+    if (new Date().getSeconds() % 3 == 0) {
+      throw new Error("INTERNAL_SERVER_ERROR");
+    }
+
+    return { ...(await char.json()), __typename: "StarWarsCharacter" };
   },
 };
 
 const StarWarsCharacter = {
-  name: () => {
-    return "test";
+  //@ts-ignore
+  name: (parent) => {
+    return parent.name;
   },
-  height: () => {
-    return "171";
-  },
-  mass: () => {
-    return "100";
-  },
-  gender: () => {
-    return "MALE";
+  // height: () => {
+  //   return "171";
+  // },
+  // mass: () => {
+  //   return "100";
+  // },
+  //@ts-ignore
+  gender: (parent) => {
+    return parent.gender.toUpperCase();
   },
 };
 
