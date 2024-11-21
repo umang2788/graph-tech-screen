5fd6c503598a25dbe2173477dd3c5b3cfeb643df
umang2788 <umang2788@users.noreply.github.com>
2024-11-20 23:47:46 -0600

    Update arrays.ts

--- src/routes/arrays.ts
+++ src/routes/arrays.ts
@@ -5,16 +5,14 @@ export const route = (req: Request, res: Response) => {
   const sampleArray = [1, 2, 3, 4, 5];
 
   // Perform some array operations
-  const doubledArray = sampleArray.map((num) => num * 2);
-  const filteredArray = sampleArray.filter((num) => num > 2);
-  const sum = sampleArray.reduce((acc, num) => acc + num, 0);
+
 
   // Send the results as a response
   res.json({
-    original: sampleArray,
-    doubled: doubledArray,
-    filtered: filteredArray,
-    sum: sum,
+    original: ,
+    doubled: ,
+    filtered: ,
+    sum: ,
   });
 };
 
